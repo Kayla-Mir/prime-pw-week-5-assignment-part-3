6d767eef97d0b8524e72f209aaead9e2a5edeeac
Kayla Mir <kayla.mir32@gmail.com>
2021-10-19 11:25:35 -0500

    function to find by artist completed

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -12,12 +12,12 @@ function addToCollection(title, artist, yearPublished) {
   return awesomeAlbum;
 }
 
-console.log('Adding a new album ', addToCollection('reality in BLACK', 'Mamamoo', '2019'));
-console.log('Adding a new album ', addToCollection('MORE & MORE', 'TWICE', '2020'));
-console.log('Adding a new album ', addToCollection('Lemonade', 'Beyonce', '2016'));
-console.log('Adding a new album ', addToCollection('The Fame Monster', 'Lady Gaga', '2009'));
-console.log('Adding a new album ', addToCollection('beerbongs & bentleys', 'Post Malone', '2018'));
-console.log('Adding a new album ', addToCollection('Back to Black', 'Amy Winehouse', '2006'));
+console.log('Adding a new album:', addToCollection('reality in BLACK', 'Mamamoo', '2019'));
+console.log('Adding a new album:', addToCollection('MORE & MORE', 'TWICE', '2020'));
+console.log('Adding a new album:', addToCollection('Lemonade', 'Beyonce', '2016'));
+console.log('Adding a new album:', addToCollection('The Fame Monster', 'Lady Gaga', '2009'));
+console.log('Adding a new album:', addToCollection('beerbongs & bentleys', 'Post Malone', '2018'));
+console.log('Adding a new album:', addToCollection('Feel Special', 'TWICE', '2019'));
 
 console.log('My collection:', collection);
 
@@ -28,3 +28,16 @@ function showCollection(array) {
   }
 }
 showCollection(collection);
+
+function findByArtist(artist) {
+  let isThisYourArtist = [];
+  for (i = 0; i < collection.length; i++) {
+    if (collection[i].artist === artist) {
+      isThisYourArtist.push(collection[i]);
+    }
+  }
+  return isThisYourArtist;
+}
+
+console.log('Do I have TWICE?:', findByArtist('TWICE'));
+console.log('Do I have Purple Kiss?:', findByArtist('Purple Kiss'));
