e30d6a26ff8675d508771b1ddd791c1f608103fc
Kayla Mir <kayla.mir32@gmail.com>
2021-10-19 10:34:51 -0500

    set up a function to add albums and tested it with my 6 favorite albums

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,1 +1,20 @@
 console.log('***** Music Collection *****')
+
+let collection = [];
+
+function addToCollection(title, artist, yearPublished) {
+  let awesomeAlbum = {
+    title: title,
+    artist: artist,
+    yearPublished: yearPublished,
+  }
+  collection.push(awesomeAlbum);
+  return awesomeAlbum;
+}
+
+console.log('Adding a new album ', addToCollection('reality in BLACK', 'Mamamoo', '2019'));
+console.log('Adding a new album ', addToCollection('MORE & MORE', 'TWICE', '2020'));
+console.log('Adding a new album ', addToCollection('Lemonade', 'Beyonce', '2016'));
+console.log('Adding a new album ', addToCollection('The Fame Monster', 'Lady Gaga', '2009'));
+console.log('Adding a new album ', addToCollection('beerbongs & bentleys', 'Post Malone', '2018'));
+console.log('Adding a new album ', addToCollection('Back to Black', 'Amy Winehouse', '2006'));
