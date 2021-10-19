3f1e7c51042f996ac7d3ab153c55a7bf654e00f9
Kayla Mir <kayla.mir32@gmail.com>
2021-10-19 10:48:56 -0500

    finished and tested the showCollection function

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -18,3 +18,13 @@ console.log('Adding a new album ', addToCollection('Lemonade', 'Beyonce', '2016'
 console.log('Adding a new album ', addToCollection('The Fame Monster', 'Lady Gaga', '2009'));
 console.log('Adding a new album ', addToCollection('beerbongs & bentleys', 'Post Malone', '2018'));
 console.log('Adding a new album ', addToCollection('Back to Black', 'Amy Winehouse', '2006'));
+
+console.log('My collection:', collection);
+
+function showCollection(array) {
+  console.log(array.length);
+  for (i = 0; i < array.length; i++) {
+    console.log(`${array[i].title} by ${array[i].artist}, published in ${array[i].yearPublished}.`);
+  }
+}
+showCollection(collection);
