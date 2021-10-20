303709f423e587098cad80e2748eb2154fb957b7
Kayla Mir <kayla.mir32@gmail.com>
2021-10-20 12:44:24 -0500

    worked on stretch goals, i need to update the showCollection function next

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -2,30 +2,140 @@ console.log('***** Music Collection *****')
 
 let collection = [];
 
-function addToCollection(title, artist, yearPublished) {
+function addToCollection(title, artist, yearPublished, tracks) {
   let awesomeAlbum = {
     title: title,
     artist: artist,
     yearPublished: yearPublished,
+    tracks: tracks
   }
   collection.push(awesomeAlbum);
   return awesomeAlbum;
 }
 
-console.log('Adding a new album:', addToCollection('reality in BLACK', 'Mamamoo', '2019'));
-console.log('Adding a new album:', addToCollection('MORE & MORE', 'TWICE', '2020'));
-console.log('Adding a new album:', addToCollection('Lemonade', 'Beyonce', '2016'));
-console.log('Adding a new album:', addToCollection('The Fame Monster', 'Lady Gaga', '2009'));
-console.log('Adding a new album:', addToCollection('beerbongs & bentleys', 'Post Malone', '2018'));
-console.log('Adding a new album:', addToCollection('Feel Special', 'TWICE', '2019'));
+console.log('Adding a new album:', addToCollection(
+  'reality in BLACK',
+  'Mamamoo',
+  '2019',
+  [
+    {
+      name: 'Universe',
+      duration: '3:36'
+    },
+    {
+      name: 'Destiny',
+      duration: '4:08'
+    },
+    {
+      name: 'HIP',
+      duration: '3:15'
+    }
+  ]
+));
+console.log('Adding a new album:', addToCollection(
+  'MORE & MORE',
+  'TWICE',
+  '2020',
+  [
+    {
+      name: 'MORE & MORE',
+      duration: '3:20'
+    },
+    {
+      name: 'FIREWORK',
+      duration: '3:13'
+    },
+    {
+      name: 'OXYGEN',
+      duration: '3:45'
+    }
+  ]
+));
+console.log('Adding a new album:', addToCollection(
+  'Lemonade',
+  'Beyonce',
+  '2016',
+  [
+    {
+      name: 'Formation',
+      duration: '3:26'
+    },
+    {
+      name: 'Sorry',
+      duration: '3:53'
+    },
+    {
+      name: 'Daddy Lessons',
+      duration: '4:47'
+    }
+  ]
+));
+console.log('Adding a new album:', addToCollection(
+  'The Fame Monster',
+  'Lady Gaga',
+  '2009',
+  [
+    {
+      name: 'Bad Romance',
+      duration: '4:54'
+    },
+    {
+      name: 'Teeth',
+      duration: '3:40'
+    },
+    {
+      name: 'Speechless',
+      duration: '4:30'
+    }
+  ]
+));
+console.log('Adding a new album:', addToCollection(
+  'beerbongs & bentleys',
+  'Post Malone',
+  '2018',
+  [
+    {
+      name: 'Rockstar(feat. 21 Savage)',
+      duration: '3:38'
+    },
+    {
+      name: 'Better Now',
+      duration: '3:51'
+    },
+    {
+      name: 'Paranoid',
+      duration: '3:41'
+    }
+  ]
+));
+console.log('Adding a new album:', addToCollection(
+  'Feel Special',
+  'TWICE',
+  '2019',
+  [
+    {
+      name: 'Feel Special',
+      duration: '3:26'
+    },
+    {
+      name: 'GET LOUD',
+      duration: '3:06'
+    },
+    {
+      name: '21:29',
+      duration: '3:48'
+    }
+  ]
+));
 
 console.log('My collection:', collection);
 
 function showCollection(myArray) {
-  console.log(myArray.length);
   for (i = 0; i < myArray.length; i++) {
-    console.log(`${myArray[i].title} by ${myArray[i].artist}, published in
-      ${myArray[i].yearPublished}.`);
+    console.log(`${myArray[i].title} by ${myArray[i].artist} published in ${myArray[i].yearPublished}:
+      1. ${myArray[i].tracks[0].name}: ${myArray[i].tracks[0].duration}
+      2. ${myArray[i].tracks[1].name}: ${myArray[i].tracks[1].duration}
+      3. ${myArray[i].tracks[2].name}: ${myArray[i].tracks[2].duration}`);
   }
 }
 showCollection(collection);
@@ -51,6 +161,17 @@ function search(searchObject) {
     } else if (collection[i].artist === searchObject.artist &&
       collection[i].yearPublished === searchObject.yearPublished) {
       allMatchingItems.push(collection[i]);
+    } else if (searchObject.trackName) {
+      for (let track of collection[i].tracks) {
+        if (track.name === searchObject.trackName) {
+          allMatchingItems.push(collection[i]);
+          console.log(`Found ${searchObject.trackName} in the album ${collection[i].title}.`);
+          return allMatchingItems;
+        }
+      }
+      if (i === collection.length - 1 && allMatchingItems.length === 0) {
+        console.log(`Couldn't find: ${searchObject.trackName}`);
+      }
     }
   }
   return allMatchingItems;
@@ -65,3 +186,7 @@ console.log('Should return an empty array:', search(
 ));
 
 console.log('Should return the full collection:', search({}));
+
+console.log('Searching for song:', search({trackName: 'Paranoid'}));
+
+console.log('Searching for song:', search({trackName: 'Viva la Vida'}));
